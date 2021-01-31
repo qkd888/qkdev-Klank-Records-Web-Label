0869244af6e9d80506b44c6745e384b8592a79f2
Quintin Kelly Dekker <quintinkellydekker888@gmail.com>
2021-01-31 14:24:49 +0200

    update

---
+++ js/custom.js
@@ -0,0 +1,42 @@
+
+  $(function () {
+
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    // AOS ANIMATION
+    AOS.init({
+      disable: 'mobile',
+      duration: 800,
+      anchorPlacement: 'center-bottom'
+    });
+
+
+    // SMOOTHSCROLL NAVBAR
+    $(function() {
+      $('.navbar a, .hero-text a').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+        }, 1000);
+        event.preventDefault();
+      });
+    });    
+  });
+
+  //--------------- spinning KR logo----------------------------
+
+const image = document.querySelector('img');
+
+let spinning = true;
+degrees = 0;
+
+setInterval(() => {
+    degrees++;
+    image.style.transform = 'rotate(' + degrees + 'deg)';
+}, 18.5);
+
+    
+
